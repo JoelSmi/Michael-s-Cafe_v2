4344b5a7c2aef9e8343950513b07b02f35078bb0
Noah Nixon <blockyx@gmail.com>
2020-12-05 23:52:37 -0500

    Basics for Delivery Screens

--- Eric's Half/Delivery_Final.Designer.cs
+++ Eric's Half/Delivery_Final.Designer.cs
@@ -35,174 +35,185 @@ namespace Software_Engineering
         [DebuggerStepThrough()]
         private void InitializeComponent()
         {
-            Label1 = new Label();
-            Button1 = new Button();
-            TextBox2 = new TextBox();
-            TextBox1 = new TextBox();
-            TextBox3 = new TextBox();
-            TextBox4 = new TextBox();
-            TextBox5 = new TextBox();
-            Label2 = new Label();
-            Button2 = new Button();
-            Label3 = new Label();
-            Label13 = new Label();
-            SuspendLayout();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.BackButton = new System.Windows.Forms.Button();
+            this.PhoneNumberText = new System.Windows.Forms.TextBox();
+            this.CustomerNameText = new System.Windows.Forms.TextBox();
+            this.OrderNumberText = new System.Windows.Forms.TextBox();
+            this.DeliverNameText = new System.Windows.Forms.TextBox();
+            this.DeliverAddrText = new System.Windows.Forms.TextBox();
+            this.CustomerAddressText = new System.Windows.Forms.Label();
+            this.DoneButton = new System.Windows.Forms.Button();
+            this.OrderedItemsText = new System.Windows.Forms.Label();
+            this.TotalAmountText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
             // 
             // Label1
             // 
-            Label1.AutoSize = true;
-            Label1.Font = new Font("Microsoft YaHei", 20.25f, FontStyle.Bold, GraphicsUnit.Point, Conversions.ToByte(0));
-            Label1.ForeColor = SystemColors.ControlLightLight;
-            Label1.Location = new Point(215, 38);
-            Label1.Name = "Label1";
-            Label1.Size = new Size(127, 36);
-            Label1.TabIndex = 10;
-            Label1.Text = "Delivery";
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Label1.Location = new System.Drawing.Point(215, 38);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(127, 36);
+            this.Label1.TabIndex = 10;
+            this.Label1.Text = "Delivery";
             // 
-            // Button1
+            // BackButton
             // 
-            Button1.BackColor = Color.FromArgb(Conversions.ToInteger(Conversions.ToByte(255)), Conversions.ToInteger(Conversions.ToByte(255)), Conversions.ToInteger(Conversions.ToByte(192)));
-            Button1.FlatStyle = FlatStyle.Popup;
-            Button1.Font = new Font("Microsoft YaHei", 8.25f, FontStyle.Bold, GraphicsUnit.Point, Conversions.ToByte(0));
-            Button1.ForeColor = SystemColors.ControlText;
-            Button1.Location = new Point(34, 52);
-            Button1.Name = "Button1";
-            Button1.Size = new Size(75, 25);
-            Button1.TabIndex = 9;
-            Button1.Text = "< Back";
-            Button1.UseVisualStyleBackColor = false;
+            this.BackButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.BackButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BackButton.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BackButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.BackButton.Location = new System.Drawing.Point(34, 52);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(75, 25);
+            this.BackButton.TabIndex = 9;
+            this.BackButton.Text = "< Back";
+            this.BackButton.UseVisualStyleBackColor = false;
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
             // 
-            // TextBox2
+            // PhoneNumberText
             // 
-            TextBox2.Font = new Font("Microsoft YaHei", 8.25f, FontStyle.Regular, GraphicsUnit.Point, Conversions.ToByte(0));
-            TextBox2.Location = new Point(218, 96);
-            TextBox2.Name = "TextBox2";
-            TextBox2.Size = new Size(100, 22);
-            TextBox2.TabIndex = 13;
-            TextBox2.Text = "Phone Number";
-            TextBox2.TextAlign = HorizontalAlignment.Center;
+            this.PhoneNumberText.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PhoneNumberText.Location = new System.Drawing.Point(218, 96);
+            this.PhoneNumberText.Name = "PhoneNumberText";
+            this.PhoneNumberText.Size = new System.Drawing.Size(100, 22);
+            this.PhoneNumberText.TabIndex = 13;
+            this.PhoneNumberText.Text = "Phone Number";
+            this.PhoneNumberText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.PhoneNumberText.TextChanged += new System.EventHandler(this.PhoneNumberText_TextChanged);
             // 
-            // TextBox1
+            // CustomerNameText
             // 
-            TextBox1.Font = new Font("Microsoft YaHei", 8.25f, FontStyle.Regular, GraphicsUnit.Point, Conversions.ToByte(0));
-            TextBox1.Location = new Point(34, 96);
-            TextBox1.Name = "TextBox1";
-            TextBox1.ReadOnly = true;
-            TextBox1.Size = new Size(100, 22);
-            TextBox1.TabIndex = 12;
-            TextBox1.Text = "Customer Name";
-            TextBox1.TextAlign = HorizontalAlignment.Center;
+            this.CustomerNameText.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CustomerNameText.Location = new System.Drawing.Point(34, 96);
+            this.CustomerNameText.Name = "CustomerNameText";
+            this.CustomerNameText.ReadOnly = true;
+            this.CustomerNameText.Size = new System.Drawing.Size(100, 22);
+            this.CustomerNameText.TabIndex = 12;
+            this.CustomerNameText.Text = "Customer Name";
+            this.CustomerNameText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.CustomerNameText.TextChanged += new System.EventHandler(this.CustomerNameText_TextChanged);
             // 
-            // TextBox3
+            // OrderNumberText
             // 
-            TextBox3.Font = new Font("Microsoft YaHei", 8.25f, FontStyle.Regular, GraphicsUnit.Point, Conversions.ToByte(0));
-            TextBox3.Location = new Point(447, 96);
-            TextBox3.Name = "TextBox3";
-            TextBox3.ReadOnly = true;
-            TextBox3.Size = new Size(100, 22);
-            TextBox3.TabIndex = 39;
-            TextBox3.Text = "Order Number";
-            TextBox3.TextAlign = HorizontalAlignment.Center;
+            this.OrderNumberText.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OrderNumberText.Location = new System.Drawing.Point(447, 96);
+            this.OrderNumberText.Name = "OrderNumberText";
+            this.OrderNumberText.ReadOnly = true;
+            this.OrderNumberText.Size = new System.Drawing.Size(100, 22);
+            this.OrderNumberText.TabIndex = 39;
+            this.OrderNumberText.Text = "Order Number";
+            this.OrderNumberText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.OrderNumberText.TextChanged += new System.EventHandler(this.OrderNumberText_TextChanged);
             // 
-            // TextBox4
+            // DeliverNameText
             // 
-            TextBox4.Font = new Font("Microsoft YaHei", 8.25f, FontStyle.Regular, GraphicsUnit.Point, Conversions.ToByte(0));
-            TextBox4.Location = new Point(34, 137);
-            TextBox4.Name = "TextBox4";
-            TextBox4.ReadOnly = true;
-            TextBox4.Size = new Size(100, 22);
-            TextBox4.TabIndex = 40;
-            TextBox4.Text = "Deliverer Name";
-            TextBox4.TextAlign = HorizontalAlignment.Center;
+            this.DeliverNameText.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeliverNameText.Location = new System.Drawing.Point(34, 137);
+            this.DeliverNameText.Name = "DeliverNameText";
+            this.DeliverNameText.ReadOnly = true;
+            this.DeliverNameText.Size = new System.Drawing.Size(100, 22);
+            this.DeliverNameText.TabIndex = 40;
+            this.DeliverNameText.Text = "Deliverer Name";
+            this.DeliverNameText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.DeliverNameText.TextChanged += new System.EventHandler(this.DeliverNameText_TextChanged);
             // 
-            // TextBox5
+            // DeliverAddrText
             // 
-            TextBox5.Font = new Font("Microsoft YaHei", 8.25f, FontStyle.Regular, GraphicsUnit.Point, Conversions.ToByte(0));
-            TextBox5.Location = new Point(34, 181);
-            TextBox5.Name = "TextBox5";
-            TextBox5.Size = new Size(100, 22);
-            TextBox5.TabIndex = 41;
-            TextBox5.Text = "Delivery Address";
-            TextBox5.TextAlign = HorizontalAlignment.Center;
+            this.DeliverAddrText.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeliverAddrText.Location = new System.Drawing.Point(34, 181);
+            this.DeliverAddrText.Name = "DeliverAddrText";
+            this.DeliverAddrText.Size = new System.Drawing.Size(100, 22);
+            this.DeliverAddrText.TabIndex = 41;
+            this.DeliverAddrText.Text = "Delivery Address";
+            this.DeliverAddrText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.DeliverAddrText.TextChanged += new System.EventHandler(this.DeliverAddrText_TextChanged);
             // 
-            // Label2
+            // CustomerAddressText
             // 
-            Label2.AutoSize = true;
-            Label2.Font = new Font("Microsoft YaHei", 8.25f, FontStyle.Regular, GraphicsUnit.Point, Conversions.ToByte(0));
-            Label2.ForeColor = Color.White;
-            Label2.Location = new Point(31, 204);
-            Label2.Name = "Label2";
-            Label2.Size = new Size(126, 16);
-            Label2.TabIndex = 42;
-            Label2.Text = "*Show address chosen";
+            this.CustomerAddressText.AutoSize = true;
+            this.CustomerAddressText.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CustomerAddressText.ForeColor = System.Drawing.Color.White;
+            this.CustomerAddressText.Location = new System.Drawing.Point(31, 204);
+            this.CustomerAddressText.Name = "CustomerAddressText";
+            this.CustomerAddressText.Size = new System.Drawing.Size(126, 16);
+            this.CustomerAddressText.TabIndex = 42;
+            this.CustomerAddressText.Text = "*Show address chosen";
+            this.CustomerAddressText.Click += new System.EventHandler(this.CustomerAddressText_Click);
             // 
-            // Button2
+            // DoneButton
             // 
-            Button2.BackColor = Color.FromArgb(Conversions.ToInteger(Conversions.ToByte(255)), Conversions.ToInteger(Conversions.ToByte(255)), Conversions.ToInteger(Conversions.ToByte(192)));
-            Button2.FlatStyle = FlatStyle.Popup;
-            Button2.Font = new Font("Microsoft YaHei", 8.25f, FontStyle.Bold, GraphicsUnit.Point, Conversions.ToByte(0));
-            Button2.Location = new Point(270, 256);
-            Button2.Name = "Button2";
-            Button2.Size = new Size(75, 23);
-            Button2.TabIndex = 43;
-            Button2.Text = "Done";
-            Button2.UseVisualStyleBackColor = false;
+            this.DoneButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.DoneButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.DoneButton.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DoneButton.Location = new System.Drawing.Point(270, 256);
+            this.DoneButton.Name = "DoneButton";
+            this.DoneButton.Size = new System.Drawing.Size(75, 23);
+            this.DoneButton.TabIndex = 43;
+            this.DoneButton.Text = "Done";
+            this.DoneButton.UseVisualStyleBackColor = false;
+            this.DoneButton.Click += new System.EventHandler(this.DoneButton_Click);
             // 
-            // Label3
+            // OrderedItemsText
             // 
-            Label3.AutoSize = true;
-            Label3.Font = new Font("Microsoft YaHei", 8.25f, FontStyle.Regular, GraphicsUnit.Point, Conversions.ToByte(0));
-            Label3.ForeColor = Color.White;
-            Label3.Location = new Point(219, 137);
-            Label3.Name = "Label3";
-            Label3.Size = new Size(82, 16);
-            Label3.TabIndex = 44;
-            Label3.Text = "Ordered Items";
+            this.OrderedItemsText.AutoSize = true;
+            this.OrderedItemsText.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OrderedItemsText.ForeColor = System.Drawing.Color.White;
+            this.OrderedItemsText.Location = new System.Drawing.Point(219, 137);
+            this.OrderedItemsText.Name = "OrderedItemsText";
+            this.OrderedItemsText.Size = new System.Drawing.Size(82, 16);
+            this.OrderedItemsText.TabIndex = 44;
+            this.OrderedItemsText.Text = "Ordered Items";
+            this.OrderedItemsText.Click += new System.EventHandler(this.OrderedItemsText_Click);
             // 
-            // Label13
+            // TotalAmountText
             // 
-            Label13.AutoSize = true;
-            Label13.Font = new Font("Microsoft YaHei", 9.75f, FontStyle.Bold, GraphicsUnit.Point, Conversions.ToByte(0));
-            Label13.ForeColor = Color.White;
-            Label13.Location = new Point(215, 204);
-            Label13.Name = "Label13";
-            Label13.Size = new Size(59, 19);
-            Label13.TabIndex = 45;
-            Label13.Text = "Total: $";
+            this.TotalAmountText.AutoSize = true;
+            this.TotalAmountText.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TotalAmountText.ForeColor = System.Drawing.Color.White;
+            this.TotalAmountText.Location = new System.Drawing.Point(215, 204);
+            this.TotalAmountText.Name = "TotalAmountText";
+            this.TotalAmountText.Size = new System.Drawing.Size(59, 19);
+            this.TotalAmountText.TabIndex = 45;
+            this.TotalAmountText.Text = "Total: $";
+            this.TotalAmountText.Click += new System.EventHandler(this.TotalAmountText_Click);
             // 
             // Delivery_Final
             // 
-            AutoScaleDimensions = new SizeF(6.0f, 13.0f);
-            AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.Brown;
-            ClientSize = new Size(549, 315);
-            Controls.Add(Label13);
-            Controls.Add(Label3);
-            Controls.Add(Button2);
-            Controls.Add(Label2);
-            Controls.Add(TextBox5);
-            Controls.Add(TextBox4);
-            Controls.Add(TextBox3);
-            Controls.Add(TextBox2);
-            Controls.Add(TextBox1);
-            Controls.Add(Label1);
-            Controls.Add(Button1);
-            Name = "Delivery_Final";
-            Text = "Delivery_Final";
-            ResumeLayout(false);
-            PerformLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Brown;
+            this.ClientSize = new System.Drawing.Size(549, 315);
+            this.Controls.Add(this.TotalAmountText);
+            this.Controls.Add(this.OrderedItemsText);
+            this.Controls.Add(this.DoneButton);
+            this.Controls.Add(this.CustomerAddressText);
+            this.Controls.Add(this.DeliverAddrText);
+            this.Controls.Add(this.DeliverNameText);
+            this.Controls.Add(this.OrderNumberText);
+            this.Controls.Add(this.PhoneNumberText);
+            this.Controls.Add(this.CustomerNameText);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.BackButton);
+            this.Name = "Delivery_Final";
+            this.Text = "Delivery_Final";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         internal Label Label1;
-        internal Button Button1;
-        internal TextBox TextBox2;
-        internal TextBox TextBox1;
-        internal TextBox TextBox3;
-        internal TextBox TextBox4;
-        internal TextBox TextBox5;
-        internal Label Label2;
-        internal Button Button2;
-        internal Label Label3;
-        internal Label Label13;
+        internal Button BackButton;
+        internal TextBox PhoneNumberText;
+        internal TextBox CustomerNameText;
+        internal TextBox OrderNumberText;
+        internal TextBox DeliverNameText;
+        internal TextBox DeliverAddrText;
+        internal Label CustomerAddressText;
+        internal Button DoneButton;
+        internal Label OrderedItemsText;
+        internal Label TotalAmountText;
     }
 }
--- Eric's Half/Delivery_Final.cs
+++ Eric's Half/Delivery_Final.cs
@@ -7,5 +7,69 @@ namespace Software_Engineering
         {
             InitializeComponent();
         }
+
+        private void BackButton_Click(object sender, System.EventArgs e)
+        {
+            //If customer is guest
+            //this.Hide();
+            //new Delivery_Guest().Show();
+
+            //If customer not is guest
+            //this.Hide();
+            //new Delivery().Show();
+        }
+
+        private void DoneButton_Click(object sender, System.EventArgs e)
+        {
+            //End program
+        }
+
+        private void CustomerNameText_TextChanged(object sender, System.EventArgs e)
+        {
+            //NEEDS TO BE LABEL
+            //Retrieve customer name from file or object
+        }
+
+        private void PhoneNumberText_TextChanged(object sender, System.EventArgs e)
+        {
+            //NEEDS TO BE LABEL
+            //Retrieve customer phone number from file or object
+        }
+
+        private void OrderNumberText_TextChanged(object sender, System.EventArgs e)
+        {
+            //NEEDS TO BE LABEL
+            //Retrieve customer order number from file or object
+        }
+
+        private void DeliverNameText_TextChanged(object sender, System.EventArgs e)
+        {
+            //NEEDS TO BE LABEL
+            //Retrieve deliverer name from file or object
+        }
+
+        private void DeliverAddrText_TextChanged(object sender, System.EventArgs e)
+        {
+            //NEEDS TO BE LABEL AND COMBINED WITH CUSTOMERADDRESSTEXT
+            //Retrieve customer address from file or object
+        }
+
+        private void CustomerAddressText_Click(object sender, System.EventArgs e)
+        {
+            //NEEDS TO BE LABEL
+            //Retrieve customer address from file or object
+        }
+
+        private void OrderedItemsText_Click(object sender, System.EventArgs e)
+        {
+            //NEEDS TO BE LABEL
+            //Retrieve ordered items from file or object
+        }
+
+        private void TotalAmountText_Click(object sender, System.EventArgs e)
+        {
+            //NEEDS TO BE LABEL
+            //Retrieve total amount for order from file or object
+        }
     }
 }
